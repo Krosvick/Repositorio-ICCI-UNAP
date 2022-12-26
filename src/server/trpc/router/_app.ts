731ddf7ad3fdd4987310e3f48af790136765db1d
Krosvick <josesand99v@gmail.com>
2022-12-25 23:34:29 -0300

    added degreework router to app

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,9 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
+import { degreeWorkRouter } from "./degreeWork";
 
 export const appRouter = router({
-  example: exampleRouter,
+  degreeWork: degreeWorkRouter,
   auth: authRouter,
 });
 
