4d08cb784cc1ff4420a0c51543a5387453189825
Krosvick <josesand99v@gmail.com>
2022-12-25 23:34:11 -0300

    degreeWork router create

---
+++ src/server/trpc/router/degreeWork.ts
@@ -0,0 +1,35 @@
+import { DegreeWorkType } from "@prisma/client";
+import {z} from "zod";
+import { router,publicProcedure, protectedProcedure } from "../trpc";
+
+export const degreeWorkRouter = router({
+    publish: protectedProcedure.input(z.object({
+        title: z.string(),
+        description: z.string(),
+        //type is an enum in prisma
+        type: z.enum([DegreeWorkType.THESIS, DegreeWorkType.ARTICLE]),
+        confirmed: z.boolean(),
+        authors: z.array(z.string()),
+        advisors: z.array(z.string()),
+        year: z.number(),
+        semester: z.string(),
+        file: z.any(),
+    })).mutation(async ({input,ctx}) => {
+        const {title, description, type, confirmed, authors, advisors, year, semester, file} = input;
+        const degreeWork = await ctx.prisma.degreeWork.create({
+            data: {
+                title,
+                description,
+                type,
+                confirmed,
+                //authors is going to be an array of strings, so we need to convert it into an string separated by commas
+                authors: authors.join(","),
+                advisors: advisors.join(","),
+                year,
+                semester,
+                file,
+            }
+        });
+        return degreeWork;
+    }),     
+});
