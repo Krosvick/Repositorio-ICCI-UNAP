6784605e1fb7b52bc6ec0b9458e5148871f724cb
Krosvick <josesand99v@gmail.com>
2023-01-03 13:07:10 -0300

    email verification page

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -4,94 +4,49 @@ import {SubmitHandler, useForm} from "react-hook-form"
 import {z} from "zod"
 import {zodResolver} from "@hookform/resolvers/zod";
 import { trpc } from '../utils/trpc';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Mailform from '../components/shared/mailform';
+import Message from '../components/shared/confirmation';
 
 
-const emailSchema = z.object({
-    email: z.string().regex(/@estudiantesunap.cl$/),
+export const mailSchema = z.object({
+    email: z.string().regex(/@estudiantesunap.cl$/,{message: "Por favor, ingrese un correo institucional"})
 }) 
-type emailSchema = z.infer<typeof emailSchema>;
+export type emailSchema = z.infer<typeof mailSchema>;
 
-const verificationSchema = z.object({
-    code: z.string().min(6).max(6),
-})
-type verificationSchema = z.infer<typeof verificationSchema>;
+
 
 const emailverification: NextPage = () => {
-
+    const sendEmail = trpc.verification.sendEmail.useMutation();
     const [email, setEmail] = useState("");
     const [error, setError] = useState("");
-
-    const utils = trpc.useContext();
-        //the idea of this is to store the email when the user submits the form
-        //then hide the email input and show a text input to enter the verification code
-        //and then check if the verification code is correct
-    const {register, handleSubmit, formState:{errors}} = useForm<emailSchema>({
-        resolver: zodResolver(emailSchema),
-    });
-    const {register: register2, handleSubmit: handleSubmit2, formState:{errors: errors2}} = useForm<verificationSchema>({
-        resolver: zodResolver(verificationSchema),
-    });
-    const onSubmit: SubmitHandler<emailSchema> = async (data) => {
-        setEmail(data.email);
-        
+    const [formVisible, setFormVisible] = useState(true);
+    
+    async function handleSubmit(email: string, checkEmail:boolean | undefined) {
+        setEmail(email);
+        if(checkEmail===true){
+            setError("El correo ingresado ya se encuentra registrado");
+            setEmail("");
+            return;
+        }else if(checkEmail===false){
+            await sendEmail.mutateAsync({email});
+            setFormVisible(false);
+        }else{
+            setError("Ha ocurrido un error, por favor intente más tarde");
+            setEmail("");
+            return;
+        }
     }
-    const verifyCode: SubmitHandler<verificationSchema> = async (data) => {
-    }
+   
     return (
-        //simple centered div with an email input and a button to send the email with tailwindcss
-        <div className="flex justify-center items-center h-[calc(100vh-6rem)]">
-            <div className="flex flex-col items-center gap-3">
-                <h1 className="text-2xl font-poppins mb-4">Verifica tu correo</h1>
-                <form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit(onSubmit)}>
-                    <TextInput
-                        id="email"
-                        type="email"
-                        placeholder="Ingrese su correo institucional"
-                        required={true}
-                        sizing="lg"
-                        {...register("email",{
-                            required: "Este campo es requerido",
-                            pattern: {
-                                value: /@estudiantesunap.cl$/,
-                                message: "Ingrese un correo institucional válido",
-                            }
-                        })}
-                    />
-                    {errors && (
-                        console.log(errors),
-                        <p className="text-red-500">{errors.email?.message}</p>
-                    )}  
-                    <Button
-                        type="submit"
-                    >
-                        Enviar correo
-                    </Button>
-                </form>
-                {email && (
-                    //centered div with a text input and a button to verify the code
-                    <div className="flex flex-col items-center gap-3">
-                        <h1 className="text-2xl font-poppins mb-4">Ingresa el código de verificación</h1>
-                        <form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit2(verifyCode)}>
-                            <TextInput
-                                id="code"
-                                type="text"
-                                placeholder="Ingrese el código de verificación"
-                                required={true}
-                                sizing="lg"
-                                {...register2("code")}
-                            />
-                            <Button
-                                type="submit"
-                            >
-                                Verificar
-                            </Button>
-                        </form>
-                    </div>
-                )}
-            </div>
+        <div>
+            {formVisible ? (
+                <Mailform onSubmit={handleSubmit} error={error}/>
+            ) : (
+                <Message email = {email} onResend={() => setFormVisible(true)}/>
+            )}
         </div>
-
     ) 
 }
 
