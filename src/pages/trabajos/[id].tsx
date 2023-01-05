bdfe8c7ba3fb28259a47e87765d2efe2808c1895
Krosvick <josesand99v@gmail.com>
2023-01-05 01:54:15 -0300

    detail degreework page 60% progress

--- src/pages/trabajos/[id].tsx
+++ src/pages/trabajos/[id].tsx
@@ -3,8 +3,11 @@ import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
 import Error from "next/error";
 import { Card } from "flowbite-react"; 
+import Image from "next/image";
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 
-
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 const detalle: NextPage = () => {
     const router = useRouter();
@@ -13,10 +16,21 @@ const detalle: NextPage = () => {
     if(isLoading) return <div>Loading...</div>
     if (!data) return <Error statusCode={404} />
     return(
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center pt-10">
             <Card className="w-3/5">
-                <h1>{data.title}</h1>
-                <p>{data.description}</p>
+                <h1 className="font-poppins font-medium text-lg">{data.title}</h1>
+                <p className="font-poppins font-extralight text-sm">Fecha de publicacion: {data.year}</p>
+                <div className="flex">
+                    <div>
+                        <Document file={data.file}>
+                            <Page pageNumber={1} renderMode="canvas" scale={0.2} renderAnnotationLayer={false} renderTextLayer={false}/>
+                        </Document>
+                    </div>
+                    <div className="flex flex-col pl-5">
+                        <h3 className="mb-3">Abstracto</h3>
+                        <div className="font-poppins font-light">{data.description}</div>
+                    </div>
+                </div>
             </Card>
         </div>
     )
