76f3f28181865097702a8cc4a0dbdfffb839db14
Krosvick <josesand99v@gmail.com>
2023-01-14 00:51:15 -0300

    add framer-motion plus animations

--- src/components/workcard.tsx
+++ src/components/workcard.tsx
@@ -5,43 +5,46 @@ import Link from 'next/link';
 import { Document, Page, pdfjs } from "react-pdf";
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import {motion} from 'framer-motion';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 const WorkCard = ({work}: {work:RouterOutputs["degreeWork"]["listWorks"][number]}) => {
     const { data: sessionData } = useSession();
     return (
-        <Link href="/trabajos/[id]" as={`/trabajos/${work.id}`} className="w-10/12 h-auto">
-            <Card className="flex mb-2 rounded-none transform-gpu transition ease-in delay 100 hover:bg-gray-100">
-                <div className='flex flex-row'>
-                    <div className='flex flex-col items-start w-4/5'>
-                        <p className='font-poppins font-medium'>{work.title}</p>
-                        <p className='font-poppins font-thin text-sm'>Autores: {work.authors}</p>
-                        <p className='font-poppins font-light text-justify'>{work.description.length > 200 ?
-                        `${work.description.substring(0, 200)}...` : work.description
-                    }</p>
-                       {sessionData?.user?.role === "ADMIN" && 
-                            <div className='flex justify-between w-full items-end'>
-                                <Button>
-                                    <Link href="/edit/[id]" as={`/edit/${work.id}`}>
-                                        Editar
-                                    </Link>
-                                </Button>
-                                {work.deletedAt !== null &&
-                                <span className="bg-red-100 text-red-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">Eliminado</span>
-                                }
+        <motion.div initial={{opacity: 0, y: 10}} animate={{opacity: 1, y: 0}} exit={{opacity: 0, y: 10}} transition={{duration: 0.3}} className="w-10/12 h-auto">
+            <Link href="/trabajos/[id]" as={`/trabajos/${work.id}`}>
+                <Card className="flex mb-2 rounded-none transform-gpu hover:bg-gray-100 transition duration-300 ease-in-out">
+                    <div className='flex flex-row'>
+                        <div className='flex flex-col items-start w-4/5'>
+                            <p className='font-poppins font-medium'>{work.title}</p>
+                            <p className='font-poppins font-thin text-sm'>Autores: {work.authors}</p>
+                            <p className='font-poppins font-light text-justify'>{work.description.length > 200 ?
+                            `${work.description.substring(0, 200)}...` : work.description
+                        }</p>
+                        {sessionData?.user?.role === "ADMIN" && 
+                                <div className='flex justify-between w-full items-end'>
+                                    <Button>
+                                        <Link href="/edit/[id]" as={`/edit/${work.id}`}>
+                                            Editar
+                                        </Link>
+                                    </Button>
+                                    {work.deletedAt !== null &&
+                                    <span className="bg-red-100 text-red-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">Eliminado</span>
+                                    }
 
-                            </div>
-                        }
+                                </div>
+                            }
+                        </div>
+                        <div className='w-1/5'>
+                            <Document file={work.file}>
+                                <Page pageNumber={1} renderMode="canvas" scale={0.18} renderAnnotationLayer={false} renderTextLayer={false}/>
+                            </Document>
+                        </div>
                     </div>
-                    <div className='w-1/5'>
-                        <Document file={work.file}>
-                            <Page pageNumber={1} renderMode="canvas" scale={0.18} renderAnnotationLayer={false} renderTextLayer={false}/>
-                        </Document>
-                    </div>
-                </div>
-            </Card>
-        </Link>
+                </Card>
+            </Link>
+        </motion.div>
     );
 }
 
@@ -53,7 +56,6 @@ const DegreeWorks = () => {
     const [take, setTake] = useState(7);
     const [skip, setSkip] = useState(0);
     const {data:count} = trpc.degreeWork.count.useQuery();
-    const parent = useRef<HTMLDivElement>(null);
     //wait for sessionData to be loaded
     //prevent this Error: Too many re-renders. React limits the number of renders to prevent an infinite loop.
     const {data, status, isPreviousData, isLoading, isSuccess} = trpc.degreeWork.listWorks.useQuery({
@@ -78,11 +80,11 @@ const DegreeWorks = () => {
     }
     
     return (
-        <div className="flex flex-col justify-start items-center w-full h-full overflow-auto mb-2 pt-10">
-            {data?.map((work) => (
-                <WorkCard key={work.id} work={work}/>
-            ))}
+        <div className="flex flex-col justify-start items-center w-full h-full mb-2 pt-10 gap-4">
             {isLoading && <Spinner aria-label="Extra large spinner example "size="xl"/>}
+            {data?.map((work) => (
+                <WorkCard {...parent} key={work.id} work={work}/>
+            ))}
             {totalPages > 1 &&
                 <Pagination currentPage={currentPage} totalPages={totalPages} previousLabel="Anterior" nextLabel='Siguiente' onPageChange={(page) => {
                     setSkip((page-1)*7);
